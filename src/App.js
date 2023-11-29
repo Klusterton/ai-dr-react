7ad17bcd01a653a6dddc1636e72c716e270e0714
Emmanuel B. Giwa <iamtundegiwa@gmail.com>
2023-11-29 05:52:56 +0100

    fix build error

--- src/App.js
+++ src/App.js
@@ -1,15 +1,14 @@
 import './App.css';
 import { Route, Routes } from 'react-router-dom';
+import Landing from './Routes/Landing';
 import Auth from './Routes/Auth';
 import Dashboard from './Routes/Dashboard';
 import ChatBot from './Routes/ChatBot';
-import LandingRoutes from './Components/Landing_components/landing-routes.component';
 
 function App() {
   return (
     <Routes>
-    {/* This route is for the landing page routes. The component will house any routes that will be on the landing page, like the about us, blog, etc. */}
-      <Route exact path='*' element={<LandingRoutes />} />
+      <Route exact path='/' element={<Landing />} />
       <Route path='/auth/*' element={<Auth />} />
       <Route path='/dashboard' element={<Dashboard />} />
       <Route path='/chat-bot/*' element={<ChatBot />} />
