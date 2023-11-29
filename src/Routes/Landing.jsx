b3024b222ce61f4944c6624840e6fb84713c75fd
Emmanuel B. Giwa <iamtundegiwa@gmail.com>
2023-11-29 07:00:36 +0100

    fix potential build error

--- src/Routes/Landing.jsx
+++ src/Routes/Landing.jsx
@@ -1,11 +1,11 @@
-import React from 'react'
-import NavbarComponent from '../Components/Landing_components/navbar.component'
-import HeroSectionComponent from '../Components/Landing_components/hero-section.component'
-import FeaturesComponent from '../Components/Landing_components/features.component'
-import WhoWeAreComponent from '../Components/Landing_components/who-we-are.component'
-import HowItWorksComponent from '../Components/Landing_components/how-it-works.component'
-import FooterComponent from '../Components/Landing_components/footer.component'
-import TestimonialsComponent from '../Components/Landing_components/testimonials.component'
+import React from 'react';
+import NavbarComponent from '../Components/Landing_components/Navbar.component';
+import HeroSectionComponent from '../Components/Landing_components/hero-section.component';
+import FeaturesComponent from '../Components/Landing_components/features.component';
+import WhoWeAreComponent from '../Components/Landing_components/who-we-are.component';
+import HowItWorksComponent from '../Components/Landing_components/how-it-works.component';
+import FooterComponent from '../Components/Landing_components/footer.component';
+import TestimonialsComponent from '../Components/Landing_components/testimonials.component';
 
 export default function Landing() {
     return (
@@ -18,5 +18,5 @@ export default function Landing() {
             <TestimonialsComponent />
             <FooterComponent />
         </>
-    )
+    );
 }
